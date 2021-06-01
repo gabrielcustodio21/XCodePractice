c41d316c510f048d2f8540dd1d3e54f221221b7d
Gabriel Custodio <gkustodor@gmail.com>
2021-06-01 11:40:40 -0300

    Added base files to develop the application

---
+++ IMC/IMC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IMC
+//
+//  Created by Shinigami on 01/06/2021.
+//  Copyright © 2021 GabrielC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ IMC/IMCTests/IMCTests.swift
@@ -0,0 +1,34 @@
+//
+//  IMCTests.swift
+//  IMCTests
+//
+//  Created by Shinigami on 01/06/2021.
+//  Copyright © 2021 GabrielC. All rights reserved.
+//
+
+import XCTest
+@testable import IMC
+
+class IMCTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
